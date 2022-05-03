7fb9bc99ed196ccdc0b1e247c3829ab0cef6d4ac
Julie Nisbet <jmnisbet@gmail.com>
2022-05-03 09:50:13 -0700

    example of TDD

--- test/functions.test.js
+++ test/functions.test.js
@@ -4,11 +4,14 @@ import { myFunction } from '../functions.js';
 const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
-    const expected = true;
+    const expected = 'hi julie!';
+    const actual = myFunction('julie');
 
-    const actual = myFunction();
+    expect.equal(actual, expected, 'julie');
 
-    expect.equal(actual, expected, 'true = true');
+    const expected2 = 'hi benny!';
+    const actual2 = myFunction('benny');
+    expect.equal(actual2, expected2, 'benny');
 });
 
 skip('this test should be skipped', (expect) => {
